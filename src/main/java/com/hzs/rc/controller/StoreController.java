16ab36875984a86839187a4671d334d26cada978
hanzs <hanzs@si-tech.com.cn>
2020-03-16 20:59:00 +0800

    新增文件上传demo

--- src/main/java/com/hzs/rc/controller/StoreController.java
+++ src/main/java/com/hzs/rc/controller/StoreController.java
@@ -4,13 +4,17 @@ import com.hzs.rc.dto.StoreDTO;
 import com.hzs.rc.service.StoreService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+
 
 /**
  * @author hanzs
@@ -24,12 +28,32 @@ import javax.validation.Valid;
 public class StoreController {
     @Resource
     StoreService storeService;
-
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(StoreController.class);
     @ApiOperation(value = "添加店铺", notes = "根据参数添加店铺")
     @PostMapping(value = "/addStore")
     public void addStore(@Valid @RequestBody StoreDTO storeDTO) {
          storeService.inserStore(storeDTO);
     }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        String filePath = "D://image//store//";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            LOGGER.info("上传成功");
+            LOGGER.info("{}",dest+"/////" + contentType);
+            return "上传成功";
+        } catch (IOException e) {
+            LOGGER.error(e.toString(), e);
+        }
+        return "上传失败！";
+    }
+
 
 }
